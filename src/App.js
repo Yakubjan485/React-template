2c1e50f472f81651d5ba06679d17bbd032974320
Yakubjan485 <yakubjan99@mail.ru>
2021-08-08 13:04:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./sass/main.scss"
+import Router from "./Components/Router"
+
+const App = () => {
+  return (
+    <div>
+      <Router />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+
+const Navbar = () => {
+
+     const [show, setShow] = useState(false)
+
+     return (
+          <nav>
+               <div className="container">
+                    
+
+               </div>
+          </nav>
+     )
+}
+
+export default Navbar
